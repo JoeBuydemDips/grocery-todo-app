eea48e2685d58c2a08e3a854ed63e55192be8650
JoeBuydemDips <ksarkodie111@icloud.com>
2022-03-02 11:50:06 -0500

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onClick={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>grocery app</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="eg. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+          <button type="submit" className="submit-btn">
+            {isEditing ? "edit" : "submit"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">clear items</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
